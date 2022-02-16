bca29d5655d6a499710202f28d1f58e28bd379fa
samuel santos <samaraujo28@gmail.com>
2022-02-15 22:37:43 -0300

    Decimo requisito atendido

--- script.js
+++ script.js
@@ -26,13 +26,10 @@ function addSelectClassInTheElement() {
   });
 }
 
-function changeBackgroundColor(clickedElement, colorSelected) {
-  // Trecho basedo artigo do link:https://zellwk.com/blog/css-values-in-js/
-  const style = getComputedStyle(colorSelected);
-  const color = style.backgroundColor;
-  const element = clickedElement;
+function changeBackgroundColor(element, color) {
+  const pixel = element;
 
-  element.style.backgroundColor = color;
+  pixel.style.backgroundColor = color;
 }
 
 function changePixelColor() {
@@ -43,10 +40,12 @@ function changePixelColor() {
 
     const colorSelected = document.querySelector('.selected');
     const clickedElement = event.target;
+    // Trecho basedo artigo do link:https://zellwk.com/blog/css-values-in-js/
+    const color = getComputedStyle(colorSelected).backgroundColor;
     const hasClassPixels = clickedElement.className.includes('pixel');
 
     if (hasClassPixels) {
-      changeBackgroundColor(clickedElement, colorSelected);
+      changeBackgroundColor(clickedElement, color);
     }
   });
 }
@@ -66,6 +65,68 @@ function clearPixelBoard() {
   });
 }
 
+function createPixel() {
+  const div = document.createElement('li');
+  div.classList.add('pixel');
+
+  return div;
+}
+
+function fillLines(lines) {
+  const pixelBoard = document.querySelector('#pixel-board');
+  const line = document.createElement('div');
+  line.classList.add('line');
+
+  for (let index = 0; index < lines; index += 1) {
+    const pixel = createPixel();
+
+    line.appendChild(pixel);
+  }
+
+  pixelBoard.appendChild(line);
+}
+
+function removeOldBoard(pixelBoard) {
+  const lines = document.querySelectorAll('.line');
+
+  console.log('aqui');
+  for (let index = 0; index < lines.length; index += 1) {
+    const element = lines[index];
+    console.log(pixelBoard);
+    pixelBoard.removeChild(element);
+  }
+}
+
+function createNewPixelBoard() {
+  const generateButton = document.querySelector('#generate-board');
+  generateButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const inputValue = document.querySelector('#board-size').value;
+    const pixelBoard = document.querySelector('#pixel-board');
+
+    if (inputValue === '') {
+      alert('Board inválido!');
+      return;
+    }
+
+    removeOldBoard(pixelBoard);
+
+    for (let index = 0; index < inputValue; index += 1) {
+      fillLines(inputValue);
+    }
+  });
+}
+
+window.onload = () => {
+  const defaultNumberOfLines = 5;
+
+  for (let index = 0; index < defaultNumberOfLines; index += 1) {
+    fillLines(defaultNumberOfLines);
+  }
+};
+
 addSelectClassInTheElement();
 changePixelColor();
 clearPixelBoard();
+createNewPixelBoard();
